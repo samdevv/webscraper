be2c945387b2023491fef9f3e2d34a73b55243d9
samdevv <samdevv1@gmail.com>
2023-02-19 22:17:00 +0300

    Create SourceInterface.php

---
+++ src/Scraper/contracts/SourceInterface.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Scraper\Contracts;
+interface SourceInterface
+{
+    public function getUrl(): string;
+    public function getName(): string;
+    public function getWrapperSelector(): string;
+    public function getTitleSelector(): string;
+    public function getDescSelector(): string;
+    public function getDateSelector(): string;
+    public function getLinkSelector(): string;
+    public function getImageSelector(): string;
+}
